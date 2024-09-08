44b53ae4da311b8f321948ca6dc989c9b1b6bbfa
hyeonseok98 <157561573+hyeonseok98@users.noreply.github.com>
2024-09-08 19:28:28 +0900

    fix: api 에러 처리 추가

--- src/apis/auth/auth.api.ts
+++ src/apis/auth/auth.api.ts
@@ -1,24 +1,31 @@
 import { useAuthStore } from "@/store/auth.store";
 import { loginProps, signUpProps } from "@/types/Auth";
+import * as Sentry from "@sentry/react";
 import axiosInstance from "./axiosInstance";
 
 const ACCESS_TOKEN_EXPIRY_TIME = "30m";
 
 const login = async ({ id, password }: loginProps) => {
-  const response = await axiosInstance.post(
-    `/login?expiresIn=${ACCESS_TOKEN_EXPIRY_TIME}`,
-    {
-      id,
-      password,
+  try {
+    const response = await axiosInstance.post(
+      `/login?expiresIn=${ACCESS_TOKEN_EXPIRY_TIME}`,
+      {
+        id,
+        password,
+      }
+    );
+
+    if (response.data.success) {
+      localStorage.setItem("acToken", response.data.accessToken);
+      useAuthStore.getState().setLoggedIn(true);
     }
-  );
 
-  if (response.data.success) {
-    localStorage.setItem("acToken", response.data.accessToken);
-    useAuthStore.getState().setLoggedIn(true);
+    return response.data;
+  } catch (error) {
+    Sentry.captureException(error);
+    console.error("로그인 에러:", error);
+    throw new Error("로그인 중 에러 발생");
   }
-
-  return response.data;
 };
 
 const logout = () => {
@@ -28,13 +35,19 @@ const logout = () => {
 };
 
 const signUp = async ({ id, password, nickname }: signUpProps) => {
-  const response = await axiosInstance.post("/register", {
-    id,
-    password,
-    nickname,
-  });
+  try {
+    const response = await axiosInstance.post("/register", {
+      id,
+      password,
+      nickname,
+    });
 
-  return response.data;
+    return response.data;
+  } catch (error) {
+    Sentry.captureException(error);
+    console.error("회원가입 에러:", error);
+    throw new Error("회원가입 중 에러 발생");
+  }
 };
 
 export { login, logout, signUp };
