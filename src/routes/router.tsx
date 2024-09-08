6a5ccbb82f2ffe1a1dfeaddfe8dda28296ec1cdc
hyeonseok98 <157561573+hyeonseok98@users.noreply.github.com>
2024-09-08 19:38:34 +0900

    chore: router 상대경로 → 절대경로 변환

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,9 +1,9 @@
+import Layout from "@/components/Layouts/Layout";
+import HomePage from "@/pages/Home/HomePage";
 import LoginPage from "@/pages/Login/LoginPage";
+import MyPage from "@/pages/My/MyPage";
 import SignUpPage from "@/pages/SignUp/SignUpPage";
 import { createBrowserRouter } from "react-router-dom";
-import Layout from "../components/Layouts/Layout";
-import HomePage from "../pages/Home/HomePage";
-import MyPage from "../pages/My/MyPage";
 import ProtectedRoute from "./ProtectedRoute";
 import PublicRoute from "./PublicRoute";
 
